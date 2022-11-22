1c6ac6141d3530bbbdcb9bf5b4d6e7c49236f169
tolgaand <tolgaand@yandex.com>
2022-11-22 21:40:16 +0300

    chore: lint code

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,22 @@
-import errorHandler from "errorhandler";
-import app from "./app";
+import errorHandler from 'errorhandler'
+import app from './app'
 
 /**
  * Error Handler. Provides full stack
  */
-if (process.env.NODE_ENV === "development") {
-  app.use(errorHandler());
+if (process.env.NODE_ENV === 'development') {
+  app.use(errorHandler())
 }
 
 /**
  * Start Express server.
  */
-const server = app.listen(app.get("port"), () => {
+const server = app.listen(app.get('port'), () => {
   console.log(
-    "App is running at http://localhost:%d in %s mode",
-    app.get("port"),
-    app.get("env")
-  );
-});
+    'App is running at http://localhost:%d in %s mode',
+    app.get('port'),
+    app.get('env')
+  )
+})
 
-export default server;
+export default server
