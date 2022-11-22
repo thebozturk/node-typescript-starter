cbe64cd68617e5023efd16d5ed08724252f4ef9e
tolgaand <tolgaand@yandex.com>
2022-11-22 21:09:41 +0300

    chore: initialize husky

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,8 @@
 module.exports = {
-  globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.json"
-    }
-  },
-  moduleFileExtensions: ["ts", "js"],
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest"
+    '^.+\\.(ts|tsx)$': 'ts-jest'
   },
-  testMatch: ["**/test/**/*.test.(ts|js)"],
-  testEnvironment: "node"
-};
+  moduleFileExtensions: ['ts', 'js'],
+  testMatch: ['**/test/**/*.test.(ts|js)'],
+  testEnvironment: 'node'
+}
--- test/api.test.ts
+++ test/api.test.ts
@@ -1,8 +1,8 @@
-import request from "supertest";
-import app from "../src/app";
+import request from 'supertest'
+import app from '../src/app'
 
-describe("GET /api", () => {
-  it("should return 200 OK", () => {
-    return request(app).get("/api").expect(200);
-  });
-});
+describe('GET /api', () => {
+  it('should return 200 OK', () => {
+    return request(app).get('/').expect(200)
+  })
+})
